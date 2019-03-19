c5fd1fa46f8d75919ae1aaa2c3e9224c92469ca7
Mohammed Alshaalan <m.a.sh3lan@gmail.com>
2019-03-19 21:33:44 +0300

    add patient roles

--- contracts/medicineaccesscontrol/PatientRole.sol
+++ contracts/medicineaccesscontrol/PatientRole.sol
@@ -7,42 +7,47 @@ import "./Roles.sol";
 contract PatientRole {
 
   // Define 2 events, one for Adding, and other for Removing
+  event PatientAdded(address indexed account);
+  event patientsRemoved(address indexed account);
 
   // Define a struct 'patients' by inheriting from 'Roles' library, struct Role
+  Roles.Role private patients;
 
   // In the constructor make the address that deploys this contract the 1st patient
   constructor() public {
-    
+    _addPatient(msg.sender);
   }
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyPatient() {
-    
+    require(isPatient(msg.sender));
     _;
   }
 
   // Define a function 'isPatient' to check this role
   function isPatient(address account) public view returns (bool) {
-    
+    return manufacturers.has(account);
   }
 
   // Define a function 'addPatient' that adds this role
   function addPatient(address account) public onlyPatient {
-    
+    _addPatient(account);
   }
 
   // Define a function 'renouncePatient' to renounce this role
   function renouncePatient() public {
-    
+    _removePatient(msg.sender);
   }
 
   // Define an internal function '_addPatient' to add this role, called by 'addPatient'
   function _addPatient(address account) internal {
-    
+    patients.add(account);
+    emit PatientAdded(account);
   }
 
   // Define an internal function '_removePatient' to remove this role, called by 'removePatient'
   function _removePatient(address account) internal {
-    
+    patients.remove(account);
+    emit PatientRole(account);
   }
 }
