41df0ee969ac9dc4ccaa9eb1b7ab0993ed6215d8
Mohammed Alshaalan <m.a.sh3lan@gmail.com>
2019-03-27 17:09:42 +0300

    updating pharmacist roles modifiers and functions

--- contracts/medicineaccesscontrol/PharmacistRole.sol
+++ contracts/medicineaccesscontrol/PharmacistRole.sol
@@ -15,18 +15,18 @@ contract PharmacistRole {
   Roles.Role private pharmacists;
   // In the constructor make the address that deploys this contract the 1st pharmacist
   constructor() public {
-    _addPharmacist(msg.sender)
+    _addPharmacist(msg.sender);
   }
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyPharmacist() {
-    require(isPharmacist(msg.sender))
+    require(isPharmacist(msg.sender));
     _;
   }
 
   // Define a function 'isPharmacist' to check this role
   function isPharmacist(address account) public view returns (bool) {
-    return pharmacists[account];
+    return Roles.has(pharmacists, account);
   }
 
   // Define a function 'addPharmacist' that adds this role
@@ -41,13 +41,13 @@ contract PharmacistRole {
 
   // Define an internal function '_addPharmacist' to add this role, called by 'addPharmacist'
   function _addPharmacist(address account) internal {
-    pharmacists.add(account);
+    Roles.add(pharmacists, account);
     emit PharmacistAdded(account);
   }
 
   // Define an internal function '_removePharmacist' to remove this role, called by 'removePharmacist'
   function _removePharmacist(address account) internal {
-    pharmacists.remove(account);
+    Roles.remove(pharmacists, account);
     emit PharmacistRemoved(account);
   }
 }
