d5ce3372bf101a5d4ea5372ee4e14992a89f2f77
Mohammed Alshaalan <m.a.sh3lan@gmail.com>
2019-03-27 17:08:38 +0300

    updating manufacturer roles modifiers and functions

--- contracts/medicineaccesscontrol/ManufacturerRole.sol
+++ contracts/medicineaccesscontrol/ManufacturerRole.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.4.24;
 import "./Roles.sol";
 
 // Define a contract 'ManufacturerRole' to manage this role - add, remove, check
-contract ManufacturerRole {
+contract ManufacturerRole{
   using Roles for Roles.Role;
 
   // Define 2 events, one for Adding, and other for Removing
@@ -27,7 +27,7 @@ contract ManufacturerRole {
 
   // Define a function 'isManufacturer' to check this role
   function isManufacturer(address account) public view returns (bool) {
-    return manufacturers.has(account);
+    return Roles.has(manufacturers, account);
   }
 
   // Define a function 'addManufacturer' that adds this role
@@ -42,13 +42,13 @@ contract ManufacturerRole {
 
   // Define an internal function '_addManufacturer' to add this role, called by 'addManufacturer'
   function _addManufacturer(address account) internal {
-    manufacturers.add(account);
+    Roles.add(manufacturers, account);
     emit ManufacturerAdded(account);
   }
 
   // Define an internal function '_removeManufacturer' to remove this role, called by 'removeManufacturer'
   function _removeManufacturer(address account) internal {
-    manufacturers.remove(account);
+    Roles.remove(manufacturers, account);
     emit ManufacturerRemoved(account);
   }
 }
