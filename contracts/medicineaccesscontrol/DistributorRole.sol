e33bf1109f6fdebd6881c441bb7167872dca00ba
Mohammed Alshaalan <m.a.sh3lan@gmail.com>
2019-03-27 17:08:19 +0300

    updating distributer roles modifiers and functions

--- contracts/medicineaccesscontrol/DistributorRole.sol
+++ contracts/medicineaccesscontrol/DistributorRole.sol
@@ -5,27 +5,29 @@ import "./Roles.sol";
 
 // Define a contract 'DistributorRole' to manage this role - add, remove, check
 contract DistributorRole {
+  using Roles for Roles.Role;
 
   // Define 2 events, one for Adding, and other for Removing
   event DistributorAdded(address indexed account);
   event DistributorRemoved(address indexed account);
+
   // Define a struct 'distributors' by inheriting from 'Roles' library, struct Role
   Roles.Role private distributors;
 
   // In the constructor make the address that deploys this contract the 1st distributor
   constructor() public {
-    _addDistributor(msg.sender)
+    _addDistributor(msg.sender);
   }
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyDistributor() {
-    require(isDistributor(msg.sender))
+    require(isDistributor(msg.sender));
     _;
   }
 
   // Define a function 'isDistributor' to check this role
   function isDistributor(address account) public view returns (bool) {
-    return distributors.has(msg.sender);
+    return Roles.has(distributors, account);
   }
 
   // Define a function 'addDistributor' that adds this role
@@ -40,14 +42,14 @@ contract DistributorRole {
 
   // Define an internal function '_addDistributor' to add this role, called by 'addDistributor'
   function _addDistributor(address account) internal {
-    require(isDistributor(msg.sender));
-    distributors.add(account);
+    // require(isDistributor(msg.sender));
+    Roles.add(distributors, account);
     emit DistributorAdded(account);
   }
 
   // Define an internal function '_removeDistributor' to remove this role, called by 'removeDistributor'
   function _removeDistributor(address account) internal {
-    distributors.remove(account);
+    Roles.remove(distributors, account);
     emit DistributorRemoved(account);
   }
 }
