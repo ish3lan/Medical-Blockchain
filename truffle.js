894014475ec41dd8d7558a99618f72fee60b9700
Mohammed Alshaalan <m.a.sh3lan@gmail.com>
2019-03-31 17:05:07 +0300

    addding gitignore

--- truffle.js
+++ truffle.js
@@ -2,8 +2,13 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,
       network_id: "*" // Match any network id
     }
-  }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.24" // ex:  "0.4.20". (Default: Truffle's installed solc)
+    }
+ }
 };
